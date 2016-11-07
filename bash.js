625970a45da64c53753c8e7d1be537e2c37268dd
Harmony D <antancia@gmail.com>
2016-11-07 12:14:10 -0500

    Updated solutions

--- bash.js
+++ bash.js
@@ -1,9 +1,26 @@
-var commands = require('./commands')
+var commands = require('./commands');
+
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', function (data) {
   var cmd = data.toString().toLowerCase().trim(); // remove the newline
-  commands[cmd]();
+  
+
+  if (commands.hasOwnProperty(cmd)){
+  	commands[cmd](done);
+  } else {
+  	var cmdArr = cmd.split(" ");
+  	var rest = cmdArr.splice(1);
+  	 if (commands.hasOwnProperty(cmdArr[0])){
+  		commands[cmdArr[0]](rest,done);
+  	}
+
+  }
 
 });
+
+var done = function (output) {
+		process.stdout.write(output);
+		process.stdout.write("\nprompt > ");
+};
--- commands.js
+++ commands.js
@@ -1,25 +1,76 @@
+var fs = require('fs');
+var request = require('request');
+
 commands = {
 
-	pwd: function (){
-		process.stdout.write(__dirname);
-		process.stdout.write('\nprompt > ');
+	pwd: function (done){
+		var output ="";
+		output += __dirname;
+		done(output);
 	},
-	date: function (){
-		process.stdout.write(Date());
-		process.stdout.write('\nprompt > ');
-	}
+	date: function (done){
+		var output ="";
+		output += Date();
+		done(output);
+	},
+	ls: function (done){
+		var output = "";
+		fs.readdir('.', function(err, files) {
+	  		if (err) throw err;
+	  		files.forEach(function(file) {
+	    		output += file.toString() + "\n";
+	  		})
+	  		done(output);
+		});
+	},
+	echo: function (arr,done) {
+		var string = arr.join(" ");
+		done(string);
+	},
+	cat: function (files,done) {
+		var output = "";
+		files.forEach(function(file){
+			fs.readFile(file, function(err, data) {
+				if (err) throw err;
+				output += data;
+				done(output);
+				//process.stdout.write(data + "\n");
+				
+			});
+		});
+	},
+	head: function (file,done) {
+			var output = "";
+			fs.readFile(file[0], 'utf8', function(err, data) {
+				if (err) throw err;
+				data = data.split("\n").slice(0,10).join("\n");
+				output += data;
+				done(output);
+				
+			});
+
+	},
+	tail: function (file,done) {
+			var output = "";
+			fs.readFile(file[0], 'utf8', function(err, data) {
+				if (err) throw err;
+				data = data.split("\n");
+				data = data.slice(data.length-5,data.length).join("\n");
+				output += data;
+				done(output);
+				
+			});
+	},
+	curl: function (url,done) {
+		var output = "";
+		request(url[0], function (error, response, body) {
+  			if (!error && response.statusCode == 200) {
+    			output += body;
+  			}
+  			done(output);
+		});
+	},
+
 }
-
-
-// var commandFunc = function (cmd) {
-// 	if (cmd === "pwd") {
-	  	
-// 	  } else if (cmd === "date") {
-// 	  }else {
-// 	  	  process.stdout.write('You typed: ' + cmd);
-// 	  }
-
-// 	 process.stdout.write('\nprompt > ');
-// }
 
 module.exports = commands;
